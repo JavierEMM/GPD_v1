4eb39b2793f1ab1c242b5b9eb8bac099383e493a
Javier Marroquin <a20193644@pucp.edu.pe>
2022-12-11 02:11:32 -0500

    Falta API

--- app/src/main/java/pe/edu/pucp/gdp/Admin/AdminReportesActivity.java
+++ app/src/main/java/pe/edu/pucp/gdp/Admin/AdminReportesActivity.java
@@ -1,20 +1,82 @@
 package pe.edu.pucp.gdp.Admin;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import pe.edu.pucp.gdp.Entity.Pedido;
 import pe.edu.pucp.gdp.Login.LoginActivity;
 import pe.edu.pucp.gdp.R;
 
 public class AdminReportesActivity extends AppCompatActivity {
+
+    FirebaseFirestore firebaseFirestore =  FirebaseFirestore.getInstance();
+    ActivityResultLauncher<Intent> activityForResultLauncher = registerForActivityResult(
+       new ActivityResultContracts.StartActivityForResult(), result -> {
+        if (result.getResultCode() == Activity.RESULT_OK) {
+            Intent data = result.getData();
+            if (data != null) {
+
+                    firebaseFirestore.collection("users").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                        @Override
+                        public void onSuccess(QuerySnapshot querySnapshot) {
+                            ArrayList<String> listaIds =  new ArrayList<>();
+                            for(DocumentSnapshot documentSnapshot : querySnapshot.getDocuments()){
+                                listaIds.add(documentSnapshot.getId());
+                                Log.d("MENSAJE",documentSnapshot.getId());
+                            }
+
+                            for (String id : listaIds){
+                                firebaseFirestore.collection("users").document(id).collection("pedidos").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onSuccess(QuerySnapshot querySnapshot) {
+                                        String escribir = "";
+                                        for (DocumentSnapshot documentSnapshot : querySnapshot.getDocuments()){
+                                            Pedido pedido= documentSnapshot.toObject(Pedido.class);
+                                            if(pedido != null){
+                                                escribir +="NUMERO: "+pedido.getNumeroPedido();
+                                                escribir +="\nUSUARIO: "+pedido.getUser().getNombres();
+                                                escribir +="\nDIRECCION: "+pedido.getUser().getDireccion();
+                                                escribir +="\nPRECIO: "+pedido.getPrecio()+"\n";
+                                            }
+                                        }
+                                        try (ParcelFileDescriptor pfd = getContentResolver().openFileDescriptor(data.getData(), "w");
+                                             FileWriter fileWriter = new FileWriter(pfd.getFileDescriptor());) {
+                                            fileWriter.write(escribir);
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                    });
+            }
+        }
+    }
+);
+
 
     FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     @Override
@@ -31,6 +93,17 @@ public class AdminReportesActivity extends AppCompatActivity {
                 Intent intent =  new Intent(AdminReportesActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+        ((Button) findViewById(R.id.btnDescargaReporte)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+                    intent.addCategory(Intent.CATEGORY_OPENABLE);
+                    intent.setType("text/plain");
+                    intent.putExtra(Intent.EXTRA_TITLE,"archivo.txt");
+                    activityForResultLauncher.launch(intent);
             }
         });
     }
